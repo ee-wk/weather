51601ae6dd6195a33427445460d90b21205e2d83
ee-wk <58157140+ee-wk@users.noreply.github.com>
2021-08-08 03:19:57 +0800

    Update auto_stress_test.sh

--- jmx/auto_stress_test.sh
+++ jmx/auto_stress_test.sh
@@ -33,10 +33,10 @@ do
     fi
 
     # JMeter 静默压测
-    ${jmeter_path}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename}
+    ${JMETER_HOME}/bin/jmeter -n -t ${jmx_filename} -l ${jtl_filename}
 
     # 生成Web压测报告
-    ${jmeter_path}/bin/jmeter -g ${jtl_filename} -e -o ${web_report_path_name}
+    ${JMETER_HOME}/bin/jmeter -g ${jtl_filename} -e -o ${web_report_path_name}
 
     rm -f ${jmx_filename} ${jtl_filename}
 done
